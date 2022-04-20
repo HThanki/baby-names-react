78b0bca2729b85865d14f65f26e64c59fb2aaca9
Harsheek Thanki <harsheekthanki@gmail.com>
2022-04-20 23:21:59 +0100

    BabyNames component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import BabyNames from "./BabyNames";
 
 function App() {
   return (
     <div className="App">
       <h1>Baby Name Picker</h1>
+      <BabyNames />
     </div>
   );
 }
---
+++ src/BabyNames.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BabyNames() {
+  return (
+    <div>
+      <p>Baby Names here</p>
+    </div>
+  );
+}
+
+export default BabyNames;
