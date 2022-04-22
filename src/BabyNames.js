1cf4b45078ccef2bb574ce4aa6033a9c611d47af
Harsheek Thanki <harsheekthanki@gmail.com>
2022-04-22 23:06:45 +0100

    Level 1 challenge complete

--- src/BabyNames.js
+++ src/BabyNames.js
@@ -4,11 +4,19 @@ import babyNames from "./babyNamesData.json";
 function BabyNames() {
   return (
     <div className="baby-names">
-      {babyNames.map((babyName) => {
-        const { id, name, sex } = babyName;
-        const isBoy = sex === "m";
-        return <div className={isBoy ? "boy" : "girl"}>{name}</div>;
-      })}
+      {babyNames
+        .sort((a, z) => {
+          const nameA = a.name;
+          const nameZ = z.name;
+          if (nameA > nameZ) return 1;
+          else if (nameA < nameZ) return -1;
+          else 0;
+        })
+        .map((babyName) => {
+          const { id, name, sex } = babyName;
+          const isBoy = sex === "m";
+          return <div className={isBoy ? "boy" : "girl"}>{name}</div>;
+        })}
     </div>
   );
 }
